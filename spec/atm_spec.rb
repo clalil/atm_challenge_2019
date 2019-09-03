d0c5ede9411179006ab96559cfef342c1da93637
miyesier <miyesier@gmail.com>
2019-09-03 10:46:38 +0200

    we want order only if there are sufficient funds in the accountin order to ensure

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -16,5 +16,9 @@ describe Atm do
       subject.withdraw 50
       expect(subject.funds).to eq 950
   end
-
+  
+  it 'allow withdraw if the account has enough balance.' do 
+    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+    expect(subject.withdraw(45, account)). to eq expected_output
+  end
 end
