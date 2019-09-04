2df996849549f557f27e784fd3d9c17740ce8485
miyesier <miyesier@gmail.com>
2019-09-04 16:11:02 +0200

    Add method and test for expiration date, bug not fixed

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,13 @@
 class Account
+  STANDARD_VALIDITY_YRS = 5
 
-    attr_accessor :pin_code, :exp_date, :account_status, :balance
+    def set_expire_date
+      Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    end
 
     def initialize
-      @pin_code = rand(1000..9999)  
+      @pin_code = rand(1000..9999) 
+      @exp_date = set_expire_date
     end
 
 end
