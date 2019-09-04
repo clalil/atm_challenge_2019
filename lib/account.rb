e2cfc33edfa77e628855343e6dc24a2258881470
clalil <cliljander@gmail.com>
2019-09-04 16:25:08 +0200

    Add class name to test, fix bug

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,14 @@
+require 'date'
+
 class Account
   STANDARD_VALIDITY_YRS = 5
+    attr_accessor :exp_date
 
     def set_expire_date
       Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
 
     def initialize
-      @pin_code = rand(1000..9999) 
       @exp_date = set_expire_date
     end
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,6 @@
 require './lib/account.rb'
-require 'date'
 
-describe do
+describe Account do
 
     it 'check the length of a number' do
       number = 1234
@@ -10,7 +9,7 @@ describe do
     end
     
     it 'is expected to have an expiry date on initialize' do
-      expected_date = Date.today.next_year(5).strftime("%m/%y")
+      expected_date = Date.today.next_year(5).strftime('%m/%y')
       expect(subject.exp_date).to eq expected_date
     end
     
