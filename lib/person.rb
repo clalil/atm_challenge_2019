bcdd0822c7a5ef8eb7d794d7f527328da6a8f8c5
clalil <cliljander@gmail.com>
2019-09-05 12:31:02 +0200

    Make first test for 'name on initialize' pass by adding a initialize method with instance variable name and associated method for nil or name

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,15 @@
 class Person
-  attr_accessor :name, 
+  attr_accessor :name 
   # :cash, :account
 
-  def initialize
-    @name = name
+  def initialize(attrs = {})
+    @name = set_name(attrs[:name])
     # @cash = cash
     # @account = account
   end
+
+  def set_name(obj)
+    obj == nil ? nil : obj
+  end
+
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,10 +1,9 @@
 require './lib/person'
 require './lib/atm.rb'
-require './lib/account.rb'
 
 describe Person do
 
-    subject { described_class.new(name: 'Thomas') }
+    subject { described_class.new( name: 'Thomas') }
 
     it 'is expected to have a :name on initialize' do
         expect(subject.name).not_to be nil
